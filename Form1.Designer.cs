62d6b22228c6d6e2a3871ef2d59fc0d7a166fdfc
Gardenius <tomeksolyszko@gmail.com>
2018-12-19 19:29:43 +0100

    Signed-off-by: Gardenius <tomeksolyszko@gmail.com>

---
+++ Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Wymagana metoda obsługi projektanta — nie należy modyfikować 
+        /// zawartość tej metody z edytorem kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("/");
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Server_text = new System.Windows.Forms.TextBox();
+            this.Username_text = new System.Windows.Forms.TextBox();
+            this.Port_text = new System.Windows.Forms.TextBox();
+            this.Password_text = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.treeView = new System.Windows.Forms.TreeView();
+            this.Status_text = new System.Windows.Forms.RichTextBox();
+            this.Connect_btn = new System.Windows.Forms.Button();
+            this.Files_lst = new System.Windows.Forms.ListView();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.SuspendLayout();
+            // 
+            // Server_text
+            // 
+            this.Server_text.Location = new System.Drawing.Point(59, 12);
+            this.Server_text.Name = "Server_text";
+            this.Server_text.Size = new System.Drawing.Size(100, 20);
+            this.Server_text.TabIndex = 0;
+            // 
+            // Username_text
+            // 
+            this.Username_text.Location = new System.Drawing.Point(229, 12);
+            this.Username_text.Name = "Username_text";
+            this.Username_text.Size = new System.Drawing.Size(100, 20);
+            this.Username_text.TabIndex = 0;
+            // 
+            // Port_text
+            // 
+            this.Port_text.Location = new System.Drawing.Point(538, 12);
+            this.Port_text.Name = "Port_text";
+            this.Port_text.Size = new System.Drawing.Size(102, 20);
+            this.Port_text.TabIndex = 0;
+            // 
+            // Password_text
+            // 
+            this.Password_text.Location = new System.Drawing.Point(397, 12);
+            this.Password_text.Name = "Password_text";
+            this.Password_text.Size = new System.Drawing.Size(100, 20);
+            this.Password_text.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Server:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(165, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Username:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(335, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Password:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(503, 15);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Port:";
+            // 
+            // treeView
+            // 
+            this.treeView.ImageIndex = 0;
+            this.treeView.ImageList = this.imageList1;
+            this.treeView.Location = new System.Drawing.Point(15, 140);
+            this.treeView.Name = "treeView";
+            treeNode1.Name = "";
+            treeNode1.Text = "/";
+            this.treeView.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode1});
+            this.treeView.SelectedImageIndex = 0;
+            this.treeView.Size = new System.Drawing.Size(350, 176);
+            this.treeView.TabIndex = 5;
+            this.treeView.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView_AfterSelect);
+            // 
+            // Status_text
+            // 
+            this.Status_text.Location = new System.Drawing.Point(15, 38);
+            this.Status_text.Name = "Status_text";
+            this.Status_text.ReadOnly = true;
+            this.Status_text.Size = new System.Drawing.Size(706, 96);
+            this.Status_text.TabIndex = 6;
+            this.Status_text.Text = "";
+            this.Status_text.TextChanged += new System.EventHandler(this.Status_text_TextChanged);
+            // 
+            // Connect_btn
+            // 
+            this.Connect_btn.Location = new System.Drawing.Point(646, 9);
+            this.Connect_btn.Name = "Connect_btn";
+            this.Connect_btn.Size = new System.Drawing.Size(75, 23);
+            this.Connect_btn.TabIndex = 7;
+            this.Connect_btn.Text = "Connect";
+            this.Connect_btn.UseVisualStyleBackColor = true;
+            this.Connect_btn.Click += new System.EventHandler(this.Connect_btn_Click);
+            // 
+            // Files_lst
+            // 
+            this.Files_lst.Location = new System.Drawing.Point(371, 140);
+            this.Files_lst.Name = "Files_lst";
+            this.Files_lst.Size = new System.Drawing.Size(350, 176);
+            this.Files_lst.TabIndex = 8;
+            this.Files_lst.UseCompatibleStateImageBehavior = false;
+            this.Files_lst.View = System.Windows.Forms.View.List;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "dir_ico");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(730, 328);
+            this.Controls.Add(this.Files_lst);
+            this.Controls.Add(this.Connect_btn);
+            this.Controls.Add(this.Status_text);
+            this.Controls.Add(this.treeView);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Password_text);
+            this.Controls.Add(this.Port_text);
+            this.Controls.Add(this.Username_text);
+            this.Controls.Add(this.Server_text);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Server_text;
+        private System.Windows.Forms.TextBox Username_text;
+        private System.Windows.Forms.TextBox Port_text;
+        private System.Windows.Forms.TextBox Password_text;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TreeView treeView;
+        private System.Windows.Forms.RichTextBox Status_text;
+        private System.Windows.Forms.Button Connect_btn;
+        private System.Windows.Forms.ListView Files_lst;
+        private System.Windows.Forms.ImageList imageList1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,314 @@
+﻿using System;
+using System.Text;
+using System.Net.Sockets;
+using System.Windows.Forms;
+using System.Net;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Server_text.Text = "testftp123.cba.pl";
+            Username_text.Text = "ftptest1234";
+            Password_text.Text = "Ftptest1234";
+            Port_text.Text = "21";
+        }
+        private Socket FTPSocket = null, DataSock = null;
+        bool Logged = false, Changed = false;
+        private int Bytes, StatusCode, Port;
+        private Byte[] Buffer = new Byte[512];
+        private string StatusMessage = "", Result = "", Server = "", UserName = "", Password = "", Path = "/";
+        private string[] Msg;
+        private void Status_text_TextChanged(object sender, EventArgs e)
+        {
+            Status_text.ScrollToCaret();
+        }
+
+        private void Connect_btn_Click(object sender, EventArgs e)
+        {
+            treeView.Nodes[0].Nodes.Clear();
+            Server = Server_text.Text;
+            UserName = Username_text.Text;
+            Password = Password_text.Text;
+            Port = int.Parse(Port_text.Text);
+            Login();
+
+            if (Logged)
+            {
+                ParseDirNames(treeView.Nodes[0]);
+            }
+        }
+
+        private void Login()
+        {
+            if (Logged)
+                CloseConnection();
+            IPAddress remoteAddress = null;
+            IPEndPoint addressEndPoint = null;
+            Status_text.AppendText("Opening Connection to: " + Server + "\n");
+
+            try
+            {
+                FTPSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                Status_text.AppendText("Resolving IP Address\n");
+                remoteAddress = Dns.GetHostEntry(Server).AddressList[0];
+                Status_text.AppendText("IP Address Found -> " + remoteAddress.ToString() + "\n");
+                addressEndPoint = new IPEndPoint(remoteAddress, Port);
+                Status_text.AppendText("EndPoint Found -> " + addressEndPoint.ToString() + "\n");
+                FTPSocket.Connect(addressEndPoint);
+                SendCommand("USER " + UserName);
+                SendCommand("PASS " + Password);
+                Logged = true;
+                Status_text.AppendText("Connected to " + Server + "\n");
+            }
+            catch (Exception ex)
+            {
+                if (FTPSocket != null && FTPSocket.Connected)
+                {
+                    FTPSocket.Close();
+                }
+                Status_text.AppendText("Couldn't connect to remote server. " + ex.Message + "\n");
+                return;
+            }
+            ReadResponse();
+            ChangeDir(Path);
+
+        }
+        private void CloseConnection()
+        {
+            if (FTPSocket != null)
+            {
+                SendCommand("QUIT");
+            }
+            LogOut();
+        }
+
+        private void LogOut()
+        {
+            if (FTPSocket != null)
+            {
+                FTPSocket.Close();
+                FTPSocket = null;
+            }
+            Logged = false;
+        }
+
+        private void treeView_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            if (treeView.SelectedNode.Index != -1 && !string.Equals(treeView.SelectedNode.Text, "/"))
+            {
+                treeView.SelectedNode.Nodes.Clear();
+                SendCommand("CWD " + (treeView.SelectedNode.Tag.ToString()));
+                SendCommand("PWD");
+                Changed = true;
+                if (!Changed)
+                    return;
+                ParseDirNames(treeView.SelectedNode);
+            }
+        }
+
+        private void SendCommand(string msg)
+        {
+            Byte[] CommandBytes = Encoding.ASCII.GetBytes((msg + "\r\n").ToCharArray());
+            FTPSocket.Send(CommandBytes, CommandBytes.Length, 0);
+            ReadResponse();
+        }
+        private void ReadResponse()
+        {
+            StatusMessage = "";
+            Result = SplitResponse();
+            StatusCode = int.Parse(Result.Substring(0, 3));
+        }
+        private string SplitResponse()
+        {
+            try
+            {
+                while (true)
+                {
+                    Bytes = FTPSocket.Receive(Buffer, Buffer.Length, 0);
+                    StatusMessage += Encoding.ASCII.GetString(Buffer, 0, Bytes);
+                    if (Bytes < Buffer.Length)
+                        break;
+                }
+                string[] msg = StatusMessage.Split('\n');
+                if (StatusMessage.Length > 2)
+                    StatusMessage = msg[msg.Length - 2];
+                else
+                    StatusMessage = msg[0];
+                if (!StatusMessage.Substring(3, 1).Equals(" "))
+                    return SplitResponse();
+                for (int i = 0; i < msg.Length - 1; i++)
+                    Status_text.AppendText(msg[i] + "\n");
+                return StatusMessage;
+
+            }
+            catch (Exception ex)
+            {
+                Status_text.AppendText("Status : ERROR. " + ex.Message + "\n");
+                FTPSocket.Close();
+                return "";
+            }
+        }
+        private void ChangeDir(string Path)
+        {
+            if (!Logged)
+            {
+                Status_text.AppendText("Login First\n");
+                Changed = false;
+                return;
+            }
+            SendCommand("CWD " + Path);
+            SendCommand("PWD");
+            Path = StatusMessage.Split('"')[1];
+            Status_text.AppendText("Current Working Directory is " + Path + "\n");
+            Changed = true;
+        }
+        private void ParseDirNames(TreeNode ParentNode)
+        {
+            GetListFiles();
+            Files_lst.Items.Clear();
+            int Temp, Temp2, Temp3;
+            string Temp_Type = "", Temp_Name = "";
+            if (Msg.Length != 0)
+            {
+                for (int i = 0; i < Msg.Length - 1; i++)
+                {
+                    Temp_Type = "";
+                    Temp = Msg[i].ToString().IndexOf("type");
+                    Temp2 = Msg[i].ToString().IndexOf('=', Temp);
+                    Temp3 = Msg[i].ToString().IndexOf(';', Temp2);
+                    Temp_Type = Msg[i].Substring(Temp2 + 1, Temp3 - Temp2 - 1);
+                    if (Temp_Type == "dir")
+                    {
+                        Temp = Msg[i].ToString().LastIndexOf(';');
+                        Temp_Name = Msg[i].Substring(Temp + 1, Msg[i].Length - Temp - 1);
+                        AddTreeNode(ParentNode, "dir", Temp_Name);
+                    }
+                    else if (Temp_Type == "file")
+                    {
+                        Temp = Msg[i].ToString().LastIndexOf(';');
+                        Temp_Name = Msg[i].Substring(Temp + 1, Msg[i].Length - Temp - 1);
+                        AddListServerFilesItem(treeView.SelectedNode.Tag + "/" + Temp_Name.Trim(' '), Temp_Name.Trim(' '));
+                    }
+                }
+                treeView.Nodes[0].Expand();
+            }
+            else return;
+        }
+        private void AddTreeNode(TreeNode ParentNode, string NodeType, string Text)
+        {
+            TreeNode temp = new TreeNode();
+            temp.Name = Text + "_";
+            temp.Text = Text.Trim(' ');
+            if (ParentNode.Text != @"/")
+                temp.Tag = ParentNode.Tag.ToString().Trim(' ') + @"/" + Text.Trim(' ');
+            else
+                temp.Tag = @"/" + Text.Trim(' ');
+            if (NodeType == "dir")
+                temp.ImageIndex = 1;
+            ParentNode.Nodes.Add(temp);
+        }
+        private void AddListServerFilesItem(string Path, string Name)
+        {
+            Files_lst.Items.Add(Name);
+            Files_lst.Items[Files_lst.Items.Count - 1].SubItems.Add(FileSize(Path).ToString() + " Bytes");
+        }
+        private long FileSize(string FileName)
+        {
+            if (!Logged)
+            {
+                Status_text.AppendText("Login First\n");
+                return 0;
+            }
+            SendCommand("SIZE " + FileName);
+            long Filesize = long.Parse(Result.Substring(4));
+            return Filesize;
+        }
+        public string[] GetListFiles()
+        {
+            DataSock = OpenSocketForTransfer();
+            if (DataSock == null)
+            {
+                Status_text.AppendText("Socket Error\n");
+                return Msg;
+            }
+            SendCommand("MLSD");
+            StatusMessage = "";
+            DateTime timeout = DateTime.Now.AddSeconds(30);
+            while (timeout > DateTime.Now)
+            {
+                int Bytes = DataSock.Receive(Buffer, Buffer.Length, 0);
+                StatusMessage += Encoding.ASCII.GetString(Buffer, 0, Bytes);
+                if (Bytes < Buffer.Length) break;
+            }
+            Msg = StatusMessage.Replace("\r", "").Split('\n');
+            DataSock.Close();
+            if (StatusMessage.Contains("No files found"))
+                Msg = new string[] { };
+            ReadResponse();
+            if (StatusCode != 226)
+                Msg = new string[] { };
+            return Msg;
+        }
+
+
+        private Socket OpenSocketForTransfer()
+        {
+            SendCommand("PASV");
+            Socket tranferSocket = null;
+            IPEndPoint ipEndPoint = null;
+            int indx1 = Result.IndexOf('(');
+            int indx2 = Result.IndexOf(')');
+            string IpPort = Result.Substring((indx1 + 1), (indx2 - indx1) - 1);
+            int[] Parts = new int[6];
+            int PartCount = 0;
+            string Buffer = "";
+            for (int i = 0; i < IpPort.Length && PartCount <= 6; i++)
+            {
+                char chr = char.Parse(IpPort.Substring(i, 1));
+                if (char.IsDigit(chr))
+                    Buffer += chr;
+                else if (chr != ',')
+                {
+                    Status_text.AppendText("Wrong PASV result -> " + Result);
+                    return null;
+                }
+                else
+                {
+                    if (chr == ',' || i + 1 == IpPort.Length)
+                    {
+                        try
+                        {
+                            Parts[PartCount++] = int.Parse(Buffer);
+                            Buffer = "";
+                        }
+                        catch (Exception)
+                        {
+                            Status_text.AppendText("Wrong PASV result: " + Result + "\n");
+                            return null;
+                        }
+                    }
+                }
+            }
+            Parts[PartCount] = int.Parse(Buffer);
+            string ipAddress = Parts[0] + "." + Parts[1] + "." + Parts[2] + "." + Parts[3];
+            int port = (Parts[4] << 8) + Parts[5];
+            try
+            {
+                tranferSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                ipEndPoint = new IPEndPoint(IPAddress.Parse(ipAddress), port);
+                tranferSocket.Connect(ipEndPoint);
+            }
+            catch (Exception ex)
+            {
+                if (tranferSocket != null && tranferSocket.Connected) tranferSocket.Close();
+                Status_text.AppendText("Status : Can't connect to remote server ->" + ex.Message + " \n");
+                return null;
+            }
+            return tranferSocket;
+        }
+    }
+}
